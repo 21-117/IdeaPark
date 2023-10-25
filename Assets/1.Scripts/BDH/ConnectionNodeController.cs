055a32ac034fd7f2877661ff5c872585e83e2538
ByunDongHwan <63548551+byundonghwan@users.noreply.github.com>
2023-10-26 06:50:15 +0900

    2023.10.26 마인드 맵 노드 연결 완성
    1. 노드 연결 완성.

---
+++ Assets/1.Scripts/BDH/ConnectionNodeController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEditor.SearchService;
+using UnityEngine;
+using System;
+
+public class ConnectionNodeController : MonoBehaviour
+{
+
+    private GameObject obj;
+    
+    public static Action<GameObject, bool> nodeConnection; // 노드 간 라인렌더러를 연결
+    public static Action<GameObject> destroyIndexTipConnection; // 노드와 indexTip 라인렌더러 파괴 
+    public static Action<GameObject> destroyLineRenderer; // 노드에 연결된 라인렌더러 파괴 
+
+    void Start()
+    {
+        nodeConnection = (posObject, indexObject) =>
+        {
+            ConnectionNode(posObject, indexObject);
+        };
+
+        destroyIndexTipConnection = (deleteObject) =>
+        {
+            OnDestroyIndexTip(deleteObject);
+        };
+
+        destroyLineRenderer = (deleteLine) =>
+        {
+            OnDestroyLineRenderer(deleteLine); 
+        };
+
+    }
+
+    public void ConnectionNode(GameObject endPosObject, bool indexObject)
+    {
+        obj = Resources.Load<GameObject>("Prefabs/NodeConnectionLine");
+        GameObject LIneObject = Instantiate(obj, Vector3.zero, Quaternion.identity);
+        LIneObject.transform.SetParent(endPosObject.transform);
+        NodeConnectLine connectionLine = LIneObject.GetComponent<NodeConnectLine>();
+
+        // 연결할 노드의 연결 위치를 지정. 
+        connectionLine.STARTPOS = this.transform;
+        connectionLine.ENDPOSOBEJCT = endPosObject;
+
+        if (indexObject)
+        {
+            // INDEXDISTAL과 연결된 라인렌더러를 식별하기 위해 True 
+            connectionLine.INDEXDISTAL = true;
+        }
+
+    }
+
+    // 해당 노드에 연결되어 있는 라인렌더러 노드를 찾아서 삭제 
+    public void OnDestroyLineRenderer(GameObject deleteNode)
+    {
+        // 부모 LineRendererParent : 라인렌더러(자식)들을 가지고 있는 오브젝트  
+        Transform LineRendererParent = deleteNode.transform.GetChild(1);
+
+        for (int i = 0; i < LineRendererParent.childCount; i++)
+        {
+
+            // LineRendererParent의 라인렌더러 오브젝트들을 삭제. 
+            Destroy(LineRendererParent.GetChild(i).gameObject);
+
+        }
+
+
+    }
+
+
+    // INDEXDISTAL과 연결된 라인렌더러 노드를 찾아서 삭제.  
+    public void OnDestroyIndexTip(GameObject deleteNode)
+    {
+
+        for (int i = 0; i < deleteNode.transform.childCount; i++)
+        {
+
+            if (deleteNode.transform.GetChild(i).GetComponent<NodeConnectLine>().INDEXDISTAL == true)
+            {
+                Destroy(deleteNode.transform.GetChild(i).GetComponent<NodeConnectLine>().gameObject);
+            }
+
+        }
+
+    }
+
+
+
+
+}
--- Assets/1.Scripts/LGJ/XR_Direct.cs
+++ Assets/1.Scripts/LGJ/XR_Direct.cs
@@ -14,46 +14,46 @@ public class XR_Direct : XRDirectInteractor
     private Collider[] cols;
     private float createTime = 2.0f, curTime = 0f, lerpValue;
 
-    void Update()
-    {
-        cols = Physics.OverlapSphere(this.transform.position, radius, layerMask);
-        if(cols.Length == 0 && CheckFingerTip.instance.touchTip)
-        {
-            sampleBubble.SetActive(true);
-            lerpValue = Mathf.Clamp01(curTime / createTime);
-            sampleBubble.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, lerpValue);
-            curTime += Time.deltaTime;
-            if (curTime > createTime)
-            {
-                GameObject obj = Resources.Load<GameObject>("Bubble");
-                GameObject node = Instantiate(obj, fingerTip.position, Quaternion.identity);
-                curTime = 0f;
-            }
-        }
-        else
-        {
-            sampleBubble.SetActive(false);
-            curTime = 0f;
-        }
-    }
+    //void Update()
+    //{
+    //    cols = Physics.OverlapSphere(this.transform.position, radius, layerMask);
+    //    if(cols.Length == 0 && CheckFingerTip.instance.touchTip)
+    //    {
+    //        sampleBubble.SetActive(true);
+    //        lerpValue = Mathf.Clamp01(curTime / createTime);
+    //        sampleBubble.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, lerpValue);
+    //        curTime += Time.deltaTime;
+    //        if (curTime > createTime)
+    //        {
+    //            GameObject obj = Resources.Load<GameObject>("Bubble");
+    //            GameObject node = Instantiate(obj, fingerTip.position, Quaternion.identity);
+    //            curTime = 0f;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        sampleBubble.SetActive(false);
+    //        curTime = 0f;
+    //    }
+    //}
 
     [System.Obsolete]
     protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
         base.OnHoverEntered(args);
-        //MindMapController.setHover(true, args.interactableObject.transform.gameObject);
-        
+        MindMapController.setHover(true, args.interactableObject.transform.gameObject);
+
         Debug.Log("hover entered");
     }
     protected override void OnHoverExited(HoverExitEventArgs args)
     {
         base.OnHoverExited(args);
-        //MindMapController.setHover(false, null);
-        
-        if (args.interactableObject.transform.GetComponent<InstanceID>().curObjectType == ObjectType.Circle)
-        {
-            PlayerInfo.instance.cursorObject = args.interactableObject.transform.gameObject;
-        }
+       
+
+        //if (args.interactableObject.transform.GetComponent<InstanceID>().curObjectType == ObjectType.Circle)
+        //{
+        //    PlayerInfo.instance.cursorObject = args.interactableObject.transform.gameObject;
+        //}
 
         Debug.Log("hover exited");
     }
