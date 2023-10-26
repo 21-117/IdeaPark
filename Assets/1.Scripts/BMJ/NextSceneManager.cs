cdd3dd3ac988dafaa24566b9d6cc24e84f0d22b6
xalswox <qoalswo123@icloud.com>
2023-10-26 16:43:19 +0900

    Feat: 마이룸 공간 제작

---
+++ Assets/1.Scripts/BMJ/NextSceneManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneManager : MonoBehaviour
+{
+    //다음 씬으로 이동하도록 만든다.
+    public void NextScene()
+    {
+        SceneManager.LoadScene("IdeaPark_0.2v");
+    }
+}
