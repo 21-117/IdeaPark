289352dd4d6f965e4626010a496de7ab37d3e2c0
xalswox <qoalswo123@icloud.com>
2023-10-30 14:59:36 +0900

    Feat: My room Assigned 지정

--- Assets/1.Scripts/BMJ/AIButtonManager.cs
+++ Assets/1.Scripts/BMJ/AIButtonManager.cs
@@ -10,15 +10,16 @@ public class AIButtonManager : MonoBehaviour
 {
     //버튼 프리펩을 가져온다.
     public GameObject AIButton;
+
     public Transform attachTransform;
 
     //추가할 부모 Transform을 가져온다.
     public Transform parent;
 
-    // 해당 이벤트 오브젝트 가져오기 
+    // 해당 이벤트 오브젝트 가져오기
     private GameObject EventButtonObj;
 
-    // 해당 이벤트 오브젝트 데이터 
+    // 해당 이벤트 오브젝트 데이터
     private string EventButtonData;
 
     //5글자 이상인 음식과 관련된 예제 리스트 5개를 만들어보겠다.
@@ -50,56 +51,47 @@ public class AIButtonManager : MonoBehaviour
             // 버튼마다 클릭 이벤트를 설정.
             Button btn = button_i.GetComponent<Button>();
             btn.onClick.AddListener(OnClickButtonEvent);
-
         }
     }
 
-    // 해당 추천 AI 키워드 버튼 클릭 이벤트 
+    // 해당 추천 AI 키워드 버튼 클릭 이벤트
     public void OnClickButtonEvent()
     {
-
-        // 해당 이벤트 오브젝트 가져오기 
+        // 해당 이벤트 오브젝트 가져오기
         EventButtonObj = EventSystem.current.currentSelectedGameObject;
 
-        // 해당 이벤트 오브젝트에서 데이터를 가져오기 
+        // 해당 이벤트 오브젝트에서 데이터를 가져오기
         EventButtonData = EventButtonObj.GetComponentInChildren<TextMeshProUGUI>().text;
-
-
-
-
     }
 
-    // 마인드 추가 버튼 클릭 이벤트 
+    // 마인드 추가 버튼 클릭 이벤트
     public void AddMind_Btn()
     {
-        // 생성하고 싶은 키워드를 선택하고 마인드 추가 
+        // 생성하고 싶은 키워드를 선택하고 마인드 추가
         if (EventButtonObj != null)
         {
             // 마인드 해당 오브젝트 생성.
             print("마인드 노드 생성 : " + EventButtonObj.name);
-            // 마인드 노드에 데이터 삽입. 
+            // 마인드 노드에 데이터 삽입.
             print("마인드 노드 데이터 삽입 : " + EventButtonData);
             PlayerInfo.instance.createBubble = true;
             MindMapController.instance.UpdateCreate(attachTransform, EventButtonData);
             PlayerInfo.instance.createBubble = false;
-            // 링크도 연결. 
+            // 링크도 연결.
 
             // AI 키워드 추천 UI창이 사라지고 노드 바로 생성
             //this.transform.parent.gameObject.SetActive(false);
-
         }
         else
         {
             // 에러 팝업 창 : ( AI KEYWORD를 선택하지 않았습니다. ! )
         }
-
     }
 
-    // 다시 보기 버튼 클릭 이벤트 
+    // 다시 보기 버튼 클릭 이벤트
     public void Menu_Again_Btn()
     {
         print("다시 보기 버튼 클릭.");
-        // AI 서버에서 다시 키워드들을 호출한다. 
+        // AI 서버에서 다시 키워드들을 호출한다.
     }
-
 }
