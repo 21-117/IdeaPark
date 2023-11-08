0d5334d787ee8a4db6b5bfcb29e71a042b4a2a94
xalswox <qoalswo123@icloud.com>
2023-11-08 16:36:04 +0900

    Feat: UI 사용성 개선

--- Assets/1.Scripts/BMJ/NextSceneManager.cs
+++ Assets/1.Scripts/BMJ/NextSceneManager.cs
@@ -8,6 +8,6 @@ public class NextSceneManager : MonoBehaviour
     //다음 씬으로 이동하도록 만든다.
     public void NextScene()
     {
-        SceneManager.LoadScene("IdeaPark_0.2v");
+        SceneManager.LoadScene("IdeaPark_0.3v");
     }
 }
