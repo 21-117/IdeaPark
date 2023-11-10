692451d5b09a354730f1e719f0a4fb9358600f53
JLee <99634045+21-117@users.noreply.github.com>
2023-11-10 19:06:35 +0900

    Update NewtworkPlayer.cs

--- Assets/1.Scripts/Photon/NewtworkPlayer.cs
+++ Assets/1.Scripts/Photon/NewtworkPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class NewtworkPlayer : MonoBehaviour
 {
@@ -11,10 +12,18 @@ public class NewtworkPlayer : MonoBehaviour
     public Transform rightHand;
     private PhotonView photonView;
 
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
+
     // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+        XRRig rig = FindObjectOfType<XRRig>();
+        headRig = rig.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = rig.transform.Find("Camera Offset/LeftHand");
+        rightHandRig = rig.transform.Find("Camera Offset/RightHand");
     }
 
     // Update is called once per frame
@@ -25,18 +34,16 @@ public class NewtworkPlayer : MonoBehaviour
             head.gameObject.SetActive(false);
             leftHand.gameObject.SetActive(false);
             rightHand.gameObject.SetActive(false);
-            MapPosition(head, XRNode.Head);
-            MapPosition(leftHand, XRNode.LeftHand);
-            MapPosition(rightHand, XRNode.RightHand);
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
         }
     }
 
-    void MapPosition(Transform target, XRNode node)
+    void MapPosition(Transform target, Transform rigTransform)
     {
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
 
-        target.position = position;
-        target.rotation = rotation;
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
     }
 }
