a442ddd580db1f77afcd4dabd2bc15aa0aa8c4fd
ByunDongHwan <63548551+byundonghwan@users.noreply.github.com>
2023-11-24 14:53:10 +0900

    2023.11.24 Merge

---
+++ Assets/1.Scripts/BDH/05_PlayerSetting/OVRPoseTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRPoseTest : MonoBehaviour
+{
+   public void Selected()
+    {
+        print("실행되엇다.");
+    }
+
+    public void UnSelected()
+    {
+        print("풀림.");
+    }
+}
